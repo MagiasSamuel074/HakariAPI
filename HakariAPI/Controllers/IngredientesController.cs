38b4f10a7c2df7d69a159899ae15f679747dd667
MagiasSamuel074 <cervantessamuel074@hotmail.com>
2026-03-31 20:35:04 -0500

    Creacion de Controlador de Ingredientes agragando metodo GET e inyeccion de dependencia.

---
+++ HakariAPI/Controllers/IngredientesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HakariAPI.Data; //carpeta de conexion a DB
+using HakariAPI.Models; //obteniendo datos de las tablas
+using Microsoft.EntityFrameworkCore;
+
+namespace HakariAPI.Controllers
+{
+    [Route("api/[controller]")] //la direccion en internet sera: api/ingredientes.
+    [ApiController]
+    public class IngredientesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        //Aqui le pedimos a Program.cs que nos preste la conexion a DB
+        public IngredientesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //GET: api/ingredientes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Ingrediente>>> GetIngredientes()
+        {
+            //Equivalente a SELECT * FROM Ingredientes;
+            return await _context.Ingredientes.ToListAsync();
+        }
+    }
+}
