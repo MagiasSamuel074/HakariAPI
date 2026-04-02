01bb3bce75d11ad2cd4bd45ca6f1e12ead9c42af
MagiasSamuel074 <cervantessamuel074@hotmail.com>
2026-04-02 00:02:34 -0500

    Creacion de Metodo Post.

--- HakariAPI/Controllers/IngredientesController.cs
+++ HakariAPI/Controllers/IngredientesController.cs
@@ -10,20 +10,38 @@ namespace HakariAPI.Controllers
     [ApiController]
     public class IngredientesController : ControllerBase
     {
+       //se declara aqui el campo privado para almacenar el contexto de la base de datos.
         private readonly AppDbContext _context;
 
-        //Aqui le pedimos a Program.cs que nos preste la conexion a DB
+        //Aqui le pedimos a Program.cs que nos preste la conexion a DB para almacenarlo o inyectarlo al campo
         public IngredientesController(AppDbContext context)
         {
             _context = context;
         }
 
         //GET: api/ingredientes
+        //Equivalente a SELECT
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Ingrediente>>> GetIngredientes()
         {
             //Equivalente a SELECT * FROM Ingredientes;
             return await _context.Ingredientes.ToListAsync();
+        }
+
+        //Equivalente a INSERT INTO
+        //POST: api/ingredientes
+        [HttpPost]
+        public async Task<ActionResult<Ingrediente>> PostIngrediente (Ingrediente nuevoIngrediente)
+        {
+
+            //Le decimos a EF que procese la info de este objeto para la tabla ingreidientes
+            _context.Ingredientes.Add(nuevoIngrediente);
+
+            //se guarda en la tabla y es equivalente a COMMIT en SQL
+            await _context.SaveChangesAsync();
+
+           // Respondemos que se creó con éxito y mandamos el objeto de vuelta
+            return CreatedAtAction(nameof(GetIngredientes), new { id = nuevoIngrediente.Idingrediente }, nuevoIngrediente);
         }
     }
 }
