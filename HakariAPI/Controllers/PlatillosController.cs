beda728ac8e3cf2f83e65b2bb2d7f9976e0e9aad
MagiasSamuel074 <cervantessamuel074@hotmail.com>
2026-04-13 22:11:54 -0500

    Creacion de PlatillosController junto con la inyrccion de dependencias.

---
+++ HakariAPI/Controllers/PlatillosController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HakariAPI.Data;
+using HakariAPI.Models;
+
+namespace HakariAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatillosController : ControllerBase
+    {
+        //inyrccion de dependencia de la clase AppDbContext a la clase PlatillosController.
+        private readonly AppDbContext _context;
+
+
+        public PlatillosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
